ef33888cc7f7dd51d246f1f4c4585c7f1eac2eaa
fly-low <1803738094@qq.com>
2019-12-01 17:08:44 +0800

    gaoding

--- main.py
+++ main.py
@@ -6,48 +6,36 @@ import multiprocessing
 from multiprocessing import pool
 from db_link import RedisClient
 from spider import BiQuGe
-
+st = 0
 def gogogo(book):
-	"""
-	请求内容
-	:return:
-	# """
-	# print('开始了')
-	# print(book)
-	print('开始爬%s'%book)
-	st = time.time()
+	global st
+	st = time.time ()
+	print('开启进程%s\n'%str(book[1]))
 	spider_list = [BiQuGe(job=book) for i in range(5)]
 	for spider in spider_list:
 		spider.start()
-		# spider.joi
-	with open('biguge.log','a') as fp:
-		fp.write('%s爬完了，用了%f\n' % (book,time.time()-st))
-	print ( '%s爬完了，用了%f' % book,time.time()-st )
-
+def log(book):
+	global st
+	with open('biquge.log','a') as fp:
+		fp.write('%s爬完了，用了%f'%(book,time.time()))
+		print('%s爬完了，用了%f'%(book,time.time()))
 
 
 
 if __name__ == '__main__':
-	bqg = BiQuGe()
+	bqg = BiQuGe() #获取book列表
 	p = multiprocessing.Pool(4)
 	db = RedisClient()
-	book = db.get_task ( 'BOOK_LIST' ) # 传入书名
-	gogogo(book)
 	i = 1
 	while True:
 		i+= 1
 		try:
 			book = db.get_task('BOOK_LIST')
+			print(book)
+			p.apply_async(func=gogogo,args=(book,),callback=log)
 		except:
 			break
-		p.apply_async(func=gogogo,args=(book[0],))
-	# 	except Exception as p:
-	# # 		# print(p)
-	# 		print('故障了')
-	# 		i+=1
-	# 		time.sleep(5)
 	p.close()
 	print('%s个进程创建完毕'%i)
-	print()
-	print('%s 结束'%(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())))
 	p.join()
+	print('%s 结束'%(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())))
--- spider.py
+++ spider.py
@@ -32,11 +32,8 @@ class BiQuGe(Thread):
 
 
 	def chapters(self,title_url):
-		# print('章节')
-		# print(self.kk)
-		# print('继续')
+		print('书名：',self.job[1],'\n')
 		response = requests.get(title_url,headers = self.headers)
-		# print(response.text)
 		response.encoding='utf-8'
 		html = etree.HTML(response.text)
 		chapter_urls = html.xpath('//div[@id="list"]/dl/dd/a/@href')
@@ -48,9 +45,8 @@ class BiQuGe(Thread):
 			if len(i)==2:
 				self.db.put_task(self.job[1],i)
 
-				#(0,(title,url))
+
 	def content(self,chapter_job):
-		# print('内容')
 		index,(title,url,hash_url) = chapter_job
 		try:
 			response = requests.get ( url, headers=self.headers )
@@ -58,30 +54,26 @@ class BiQuGe(Thread):
 			html = etree.HTML ( response.text )
 			contents=html.xpath('//div[@id="content"]/text()')
 			content = ''.join([i.strip() for i in contents])
-			# print ( title )
+			print ('%s：%s'%(self.job[1],title) )
 			mg = MG(MONGO_DB,self.job[1])
 			mg.table.update({'hash_url':hash_url},{'$set':{'title':title,'index':index,'content':content}},upsert=True)
 		except:
 			print('%s内容失败，存入任务列表'%title)
 			self.db.put_task(self.job[1],chapter_job)
 		# 放入失败的章节
-		# print(len(content))
 
 	def books(self,main_url):
-		# print('books')
 		response = requests.get(main_url,headers = self.headers)
 		res = etree.HTML(response.text)
 		books_url = res.xpath('//div[@id="main"]/div/ul/li/a/@href')
 		books_title = res.xpath('//div[@id="main"]/div/ul/li/a/text()')
+		# print(books_title)
 		books = zip(books_url,books_title)
-		# print('书本')
 		for i in books:
 			if len(i)==2:
 				self.db.put_task('BOOK_LIST',i)
-		# print('-----------------------------------')/
 
 	def run(self):
-		# print('run了')
 		while True:
 			chapter = self.db.get_task(self.job[1])
 			if not chapter:
